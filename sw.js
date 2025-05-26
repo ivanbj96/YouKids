d30fa04f0db20cd7b24fa2c9366258f7181fc840
Ivan <ivan@example.com>
2025-05-25 20:27:34 -0500

    Primera versión de YouKids PWA

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("youkids-cache").then((cache) => {
+      return cache.addAll([
+        "/index.html",
+        "/shorts.html",
+        "/style.css",
+        "/app.js",
+        "/manifest.json"
+      ]);
+    })
+  );
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
